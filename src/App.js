c12bf74602ee4b772ec42cfddab8ec905ccb6248
Minenkov Sergey <minenkov.sergey42@gmail.com>
2020-11-04 02:34:54 +0300

    deleted MaterialUi, some css improvements, added navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import style from './App.module.css';
-import Button  from '@material-ui/core/Button'
 
 
 const App = (props) => {
@@ -13,7 +12,7 @@ const [history, setHistory] = useState([])
 
 
   const addNumberArea = (event) => {
-    let newsymbol = event.currentTarget.name
+    let newsymbol = event.target.value
     if (newsymbol === '+' || newsymbol === '-' || newsymbol === '*' || newsymbol === '/' || newsymbol === '^') {
       if (newsymbol === operator) {
         calculateResult()
@@ -72,7 +71,7 @@ const [history, setHistory] = useState([])
     setFirstArg(newResult)
     setSecondArg('')
     setOperator('')
-    if (secondArg) {setHistory([...history, `${firstArg} ${operator} ${secondArg} = ${newResult}`])}
+    if (secondArg) {setHistory([`${firstArg} ${operator} ${secondArg} = ${newResult}`, ...history ])}
     setNumberArea(newResult)
   }
 
@@ -97,37 +96,53 @@ const [history, setHistory] = useState([])
 
   return (
     <div className={style.appWrapper}>
+      <div className={style.navbar}>
+        <div className={style.navbarHeader}>
+        Navbar Header
+        </div>
+        <div className={style.navbarEl}>
+        Menu1
+        </div>
+        <div className={style.navbarEl}>
+        Menu2
+        </div>
+        <div className={style.navbarEl}>
+        Menu3
+        </div>
+      </div>
+      <div className={style.calculator}>
       <div className={style.numberArea} >
         <textarea  id='numberArea' value={numberArea}>0</textarea>
       </div>
       <div className={style.operands} >
-        <Button variant='contained' color='primary' onClick={addNumberArea} name='+' id='but+'>+</Button>
-        <Button variant='contained' color='primary' onClick={addNumberArea} name='-' id='but*'>-</Button>
-        <Button variant='contained' color='primary' onClick={addNumberArea} name='*' id='but*'>*</Button>
-        <Button variant='contained' color='primary' onClick={addNumberArea} name='/' id='but/'>/</Button>
-        <Button variant='contained' color='primary' onClick={addNumberArea} name='^' id='but/'>^</Button>
-        <Button variant='contained' color='primary' onClick={calculateResult} name='=' id='but='>=</Button>
+        <button onClick={addNumberArea} value='+' id='but+'>+</button>
+        <button onClick={addNumberArea} value='-' id='but*'>-</button>
+        <button onClick={addNumberArea} value='*' id='but*'>*</button>
+        <button onClick={addNumberArea} value='/' id='but/'>/</button>
+        <button onClick={addNumberArea} value='^' id='but^'>^</button>
+        <button onClick={calculateResult} value='=' id='but='>=</button>
       </div>
       <div className={style.numbers} >
-          <Button variant='contained' color='primary' onClick={addNumberArea} name='1' id='but1'>1</Button>
-          <Button variant='contained' color='primary' onClick={addNumberArea} name='2' id='but2'>2</Button>
-          <Button variant='contained' color='primary' onClick={addNumberArea} name='3' id='but3'>3</Button>
+          <button onClick={addNumberArea} value='1' id='but1'>1</button>
+          <button onClick={addNumberArea} value='2' id='but2'>2</button>
+          <button onClick={addNumberArea} value='3' id='but3'>3</button>
         
-          <Button variant='contained' color='primary' onClick={addNumberArea} name='4' id='but4'>4</Button>
-          <Button variant='contained' color='primary' onClick={addNumberArea} name='5' id='but5'>5</Button>
-          <Button variant='contained' color='primary' onClick={addNumberArea} name='6' id='but6'>6</Button>
+          <button onClick={addNumberArea} value='4' id='but4'>4</button>
+          <button onClick={addNumberArea} value='5' id='but5'>5</button>
+          <button onClick={addNumberArea} value='6' id='but6'>6</button>
         
-          <Button variant='contained' color='primary' onClick={addNumberArea} name='7' id='but7'>7</Button>
-          <Button variant='contained' color='primary' onClick={addNumberArea} name='8' id='but8'>8</Button>
-          <Button variant='contained' color='primary' onClick={addNumberArea} name='9' id='but9'>9</Button>
+          <button onClick={addNumberArea} value='7' id='but7'>7</button>
+          <button onClick={addNumberArea} value='8' id='but8'>8</button>
+          <button onClick={addNumberArea} value='9' id='but9'>9</button>
         
-          <Button variant='contained' color='primary' onClick={addNumberArea} name='.' id='but.'>.</Button>
-          <Button variant='contained' color='primary' onClick={addNumberArea} name='0' id='but0'>0</Button>
-          <Button variant='contained' color='primary' onClick={clearNumberArea} name='' id='butClear'>AC</Button>
+          <button onClick={addNumberArea} value='.' id='but.'>.</button>
+          <button onClick={addNumberArea} value='0' id='but0'>0</button>
+          <button onClick={clearNumberArea} value='' id='butClear'>AC</button>
         </div>
         <div className={style.history}> 
           <div className={style.historyHeader}>History of Calculations</div>
             {history.map( c => <div className={style.historyEl} onClick={clickHistory} value={c}> {c} </div> ) }
+        </div>
         </div>
       </div>
    
